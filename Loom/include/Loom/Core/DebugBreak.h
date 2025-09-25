50a4f559b588b2915f875b9db7189d5dafdd9a71
Tanapat Namsomboon <tanapat.nams@outlook.com>
2025-09-25 15:59:43 +0700

    Enhance debug break system with improved compiler and platform support

--- Loom/include/Loom/Core/DebugBreak.h
+++ Loom/include/Loom/Core/DebugBreak.h
@@ -3,15 +3,25 @@
 #include "Loom/Core/PlatformDetection.h"
 
 #ifdef LOOM_DEBUG
-#   if defined(LOOM_PLATFORM_WINDOWS)
+#   if defined(_MSC_VER)
+#       include <intrin.h>
 #       define LOOM_DEBUGBREAK() __debugbreak()
-#   elif defined(LOOM_PLATFORM_LINUX)
+#   elif defined(__clang__)
+#       if __has_builtin(__builtin_debugtrap)
+#           define LOOM_DEBUGBREAK() __builtin_debugtrap()
+#       else
+#           define LOOM_DEBUGBREAK() __builtin_trap()
+#       endif
+#   elif defined(__GNUC__)
+#       if defined(__i386__) || defined(__x86_64__)
+#           define LOOM_DEBUGBREAK() __asm__ volatile("int $3")
+#       else
+#           define LOOM_DEBUGBREAK() __builtin_trap()
+#       endif
+#   else
 #       include <signal.h>
 #       define LOOM_DEBUGBREAK() raise(SIGTRAP)
-#   else
-#       error "Platform doesn't support debugbreak yet!"
 #   endif
-#   define LOOM_ENABLE_ASSERTS
 #else
 #   define LOOM_DEBUGBREAK()
 #endif
